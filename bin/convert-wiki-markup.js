b4e644a4a9ada8521b74952b89150131f0beb0d4
Manuel Wieser <office@manuelwieser.com>
2018-07-17 07:51:42 +0200

    Add countries

--- bin/convert-wiki-markup.js
+++ bin/convert-wiki-markup.js
@@ -7,14 +7,16 @@ const args = process.argv.slice(2);
 const source = args[0];
 const sets = args.slice(1);
 
+const defaultRegex = /;(.*?):(.*?)\(?''(.*?)''/gm;
+const countryRegex = /;(.*?)\s*?''(.*?)''\s*?:\s*?(.*?)$/gm;
+
 function parsePhrases(markup, _sets) {
-  const regex = /;(.*?):(.*?)\(?''(.*?)''/gm;
   const phrases = {};
   let matches;
 
-  while ((matches = regex.exec(markup)) !== null) {
-    if (matches.index === regex.lastIndex) {
-      regex.lastIndex += 1;
+  while ((matches = defaultRegex.exec(markup)) !== null) {
+    if (matches.index === defaultRegex.lastIndex) {
+      defaultRegex.lastIndex += 1;
     }
 
     const id = uuidv4();
@@ -30,8 +32,30 @@ function parsePhrases(markup, _sets) {
   return phrases;
 }
 
+function parseCountries(markup, _sets) {
+  const phrases = {};
+  let matches;
+
+  while ((matches = countryRegex.exec(markup)) !== null) {
+    if (matches.index === countryRegex.lastIndex) {
+      countryRegex.lastIndex += 1;
+    }
+
+    const id = uuidv4();
+    phrases[id] = {
+      id: uuidv4(),
+      english: matches[3].trim(),
+      japanese: matches[1].trim(),
+      romaji: matches[2].trim(),
+      sets: _sets,
+    };
+  }
+
+  return phrases;
+}
+
 fs.readFile(path.join(__dirname, source), 'utf-8', (err, data) => {
   if (err) throw err;
-  const phrases = parsePhrases(data, sets);
+  const phrases = Object.assign({}, parsePhrases(data, sets), parseCountries(data, sets));
   console.log(JSON.stringify(phrases, null, 2));
 });
