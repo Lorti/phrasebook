062d561af6f0c0f9a0e0031267f6a1ea212e6e80
mwieser <manuel.wieser@karriere.at>
2018-07-27 13:10:29 +0200

    Update sitemap.xml

--- bin/sitemap-generator.js
+++ bin/sitemap-generator.js
@@ -2,6 +2,7 @@ const SitemapGenerator = require('sitemap-generator');
 
 const generator = SitemapGenerator('https://www.japanese-phrasebook.com/', {
   filepath: `${__dirname}/../root/sitemap.xml`,
+  lastMod: true,
   stripQuerystring: true,
 });
 
