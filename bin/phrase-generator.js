03c3a0c956e81c8e7d34e6e1dd68c4eb7aaad5db
Manuel Wieser <office@manuelwieser.com>
2018-07-28 14:03:17 +0200

    Add Authority, Driving and Typical Expressions topics

--- bin/phrase-generator.js
+++ bin/phrase-generator.js
@@ -8,8 +8,8 @@ const source = args[0];
 const sets = args.slice(1);
 
 const defaultRegex = /;(.*?):(.*?)\(?''(.*?)''/gm;
-const countryRegex = /;(.*?)\s*?''(.*?)''\s*?:\s*?(.*?)$/gm;
 const signsRegex = /;(.*?):(.*?)$/gm;
+const expressionsRegex = /;(.*?)\s*?''(.*?)''\s*?:\s*?(.*?)$/gm;
 
 function parsePhrases(markup, _sets) {
   const phrases = {};
@@ -26,28 +26,6 @@ function parsePhrases(markup, _sets) {
       english: matches[1].trim(),
       japanese: matches[2].trim(),
       romaji: matches[3].trim(),
-      sets: _sets,
-    };
-  }
-
-  return phrases;
-}
-
-function parseCountries(markup, _sets) {
-  const phrases = {};
-  let matches;
-
-  while ((matches = countryRegex.exec(markup)) !== null) {
-    if (matches.index === countryRegex.lastIndex) {
-      countryRegex.lastIndex += 1;
-    }
-
-    const id = uuidv4();
-    phrases[id] = {
-      id: uuidv4(),
-      english: matches[3].trim(),
-      japanese: matches[1].trim(),
-      romaji: matches[2].trim(),
       sets: _sets,
     };
   }
@@ -76,14 +54,36 @@ function parseSigns(markup, _sets) {
   return phrases;
 }
 
+function parseExpressions(markup, _sets) {
+  const phrases = {};
+  let matches;
+
+  while ((matches = expressionsRegex.exec(markup)) !== null) {
+    if (matches.index === expressionsRegex.lastIndex) {
+      expressionsRegex.lastIndex += 1;
+    }
+
+    const id = uuidv4();
+    phrases[id] = {
+      id: uuidv4(),
+      english: matches[3].trim(),
+      japanese: matches[1].trim(),
+      romaji: matches[2].trim(),
+      sets: _sets,
+    };
+  }
+
+  return phrases;
+}
+
 fs.readFile(path.join(__dirname, source), 'utf-8', (err, data) => {
   if (err) throw err;
   let phrases = parsePhrases(data, sets);
-  if (!phrases) {
-    phrases = parseSigns(data, sets);
-  }
-  if (!phrases) {
-    phrases = parseCountries(data, sets);
+  // if (!Object.keys(phrases).length) {
+  //   phrases = parseSigns(data, sets);
+  // }
+  if (!Object.keys(phrases).length) {
+    phrases = parseExpressions(data, sets);
   }
   console.log(JSON.stringify(phrases, null, 2));
 });
