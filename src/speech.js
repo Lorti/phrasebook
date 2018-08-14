bf409e83b595f5ba3bfc8e413697c8fe25412f6c
Manuel Wieser <office@manuelwieser.com>
2018-08-14 22:02:50 +0200

    Call speech synthesis on page load and cache Japanese voice, if found

--- src/speech.js
+++ src/speech.js
@@ -1,23 +1,31 @@
 let synth;
+let voice;
 
+if ('speechSynthesis' in window) {
+  synth = window.speechSynthesis;
+
+  synth.addEventListener('voiceschanged', () => {
+    const voices = synth.getVoices();
+    voice = voices.find(_voice => /ja[-_]JP/.test(_voice.lang));
+    if (!voice) {
+      // eslint-disable-next-line no-console
+      console.error(voices);
+    }
+  });
+}
+
+let status = false;
 function toggle(value) {
-  if (value && 'speechSynthesis' in window) {
-    synth = window.speechSynthesis;
-  } else {
-    synth = null;
-  }
+  status = value;
 }
 
 function speak(text) {
-  if (!synth || synth.speaking) {
-    return;
-  }
-  const voices = synth.getVoices();
-  const voice = voices.find(_voice => /ja[-_]JP/.test(_voice.lang));
-  if (!voice) {
+  if (!status || !synth || synth.speaking) {
     return;
   }
   const utterance = new SpeechSynthesisUtterance(text);
+  // eslint-disable-next-line no-console
+  utterance.addEventListener('error', error => console.error(error));
   utterance.voice = voice;
   synth.speak(utterance);
 }
