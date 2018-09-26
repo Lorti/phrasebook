4b8069271c80f6f194127856c0cd4d6dc7758e9d
Manuel Wieser <office@manuelwieser.com>
2018-09-26 22:32:29 +0200

    Fix app shell generation

--- 3/bin/shell-generator.js
+++ 3/bin/shell-generator.js
@@ -1,9 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const fs = require('fs');
+const cheerio = require('cheerio');
 
 const index = fs.readFileSync(`${__dirname}/../dist/index.html`, { encoding: 'utf8' });
 const snippet = fs.readFileSync(`${__dirname}/../public/shell.html`, { encoding: 'utf8' });
 
-const search = /<body>([^]*)<\/body>/;
-const shell = index.replace(search, snippet);
+const $ = cheerio.load(index);
+$('#app').replaceWith($(snippet));
+const shell = $.html();
 
 fs.writeFileSync(`${__dirname}/../dist/shell.html`, shell, 'utf8');
